7340289ab53df18fb86118cd837eb8ac51580730
MOH NAZIM <nazim.nn459@gmail.com>
2023-02-01 12:27:06 +0530

    add acontroller and router for home

---
+++ codeial/controllers/home_controller.js
@@ -0,0 +1,4 @@
+module.exports.home = function(req,res){
+    return res.end('<h1>express is up for codeal!</h1>');
+}
+
--- codeial/index.js
+++ codeial/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.use('/',require('./routes'));
+
 app.listen(port,function(err){
     if(err){
-        console.log('Error in running serve : ${err}');
+        console.log(`Error in running serve : ${err}`);
     }
-    console.log('Server is running on port ',port);
+    console.log(`Server is running on port :${port}`);
 });
+
 
 
 
