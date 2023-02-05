836a3d3c5e8d7a2f9043fe0ca21c6c3a282c6e3b
MOH NAZIM <nazim.nn459@gmail.com>
2023-02-06 01:45:36 +0530

    setup static files access for layout and pages

--- codeial/controllers/home_controller.js
+++ codeial/controllers/home_controller.js
@@ -1,4 +1,8 @@
-module.exports.home = function(req,res){
-    return res.end('<h1>express is up for codeal!</h1>');
+module.exports.home = function(req, res){
+    
+    return res.render('home', {
+        title: "Home"
+    });
 }
 
+// module.exports.actionName = function(req, res){}
